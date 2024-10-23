eb2f98b81ac282fe6a03759e7ae44add284bee8b
Andrew <andrewlang.p@gmail.com>
2024-10-23 16:55:25 -0400

    create connection with game server and receive data

--- play.js
+++ play.js
@@ -2,15 +2,20 @@ const net = require('net');
 
 const config = {
   host: 'localhost',
-  port: 2021
+  port: 50541
 }
 
 // establishes a connection with the game server
 const connect = function() {
   const conn = net.createConnection(config)
 
-    // interpret incoming data as text
-    conn.setEncoding("utf8");
+  // interpret incoming data as text
+  conn.setEncoding("utf8");
+
+  conn.on('data', (data) => {
+    console.log(data)
+  })
+
 
 
   return conn;
