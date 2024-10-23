fd2a5ec7d5a5c13ef5e6299db7e44a0449928e82
Andrew <andrewlang.p@gmail.com>
2024-10-23 18:44:01 -0400

    move connection function to client module

---
+++ client.js
@@ -0,0 +1,27 @@
+const net = require('net');
+
+const config = {
+  host: 'localhost',
+  port: 50541
+}
+
+// establishes a connection with the game server
+const connect = function() {
+  console.log('Connecting...')
+  const conn = net.createConnection(config)
+
+  // interpret incoming data as text
+  conn.setEncoding("utf8");
+
+  conn.on('connect', () => {
+    console.log("successfully connected to server")
+  })
+
+  conn.on('data', (data) => {
+    console.log(data)
+  })
+
+  return conn;
+}
+
+module.exports = connect;
