ca4a9501a009e759980568646c5a778d85d52da2
Andrew <andrewlang.p@gmail.com>
2024-10-23 20:23:22 -0400

    implement movement for snake

--- input.js
+++ input.js
@@ -1,5 +1,8 @@
+// Stores the active TCP connection object.
+let connection;
 
-const setupInput = function () {
+const setupInput = function (conn) {
+  connection = conn;
   const stdin = process.stdin;  // create variable to hold the stdin object so we don't have to type process.stdin multiple times
   stdin.setRawMode(true); // Raw Mode allows us to listen for individual keypresses instead of waiting for the user to press enter
   stdin.setEncoding("utf8"); // utf8 encoding is set so that we can read the text data that is input
@@ -10,6 +13,18 @@ const setupInput = function () {
 
 // setup interface to handle user input from stdin
 const handleUserInput = function(key) {
+  if (key === '\u0077') {
+    connection.write('Move: up')
+  }
+  if (key === '\u0073') {
+    connection.write('Move: down')
+  }
+  if (key === '\u0061') {
+    connection.write('Move: left')
+  }
+  if (key === '\u0064') {
+    connection.write('Move: right')
+  }
   if (key === '\u0003') {
     process.exit();
   }
--- play.js
+++ play.js
@@ -1,6 +1,6 @@
 const connect = require('./client');
 const setupInput = require('./input')
 
-connect();
 
-setupInput();
+
+setupInput(connect());
